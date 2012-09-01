3233a37b79bec3b77c5b038b6205e9c76c9564f7
Joubert RedRat <joubert@redrat.com.br>
2012-09-01 19:08:11 -0300

    Added a new version, in this version the script verify if a element is checkbox before to insert into array elements. Nova versão, nesta versão o script verifica se o elemento é um checkbox antes de inserir no array de elementos.

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 ShiftChecker
 ============
 
-ShiftChecker for jQuery v1.1
+ShiftChecker for jQuery v1.2
 
+Este plugin permite ao usuário selecionar todos os checboxs de um intervalo quando a tecla shift é pressionada.
+Um pequeno demo esta em http://www.redrat.com.br/shiftchecker/
+Testado com jQuery 1.7.2, 1.4.1 and 1.2.6.
+
+This plugin provides for user to select all checkboxs into a interval when a shiftkey is pressed.
 A small demo in http://www.redrat.com.br/shiftchecker/
-
 Tested with jQuery 1.7.2, 1.4.1 and 1.2.6.
--- shiftchecker.js
+++ shiftchecker.js
@@ -1,12 +1,12 @@
 /*!
- * ShiftChecker for jQuery v1.1
+ * ShiftChecker for jQuery v1.2                  
  *
  * Copyright (c) 2012 Joubert "RedRat" <joubert@redrat.com.br>
  * Licensed under GPL version 2
  * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  *
- * Usage: Add a class tag in yours checkboxs as you like to enable the ShiftCheck.
- * Into jQuery or document ready function, add a ShiftChecker in class that you selected.
+ * Usage: Add a class tag in yours checkboxs as you like to enable the ShiftChecker.
+ * Into your jQuery or document ready function, add a shiftChecker in class that you selected.
  * Example: $(".all").shiftChecker();
  * Enjoy and be happy.	
  */
@@ -16,23 +16,22 @@ jQuery.fn.shiftChecker = function() {
 	var count = 0;
 	var initial_element = null;
 	var final_element = null;
-	this.each(function(){
-		var element = $(this);
-		var element_value = count;
-		all_elements[count] = element;
-		element.click(function(event){
-			if(event.shiftKey)
-			{
-				initial_element = element_value > last_clicked ? last_clicked : element_value;
-				final_element = element_value > last_clicked ? element_value : last_clicked;
-				while(initial_element <= final_element)
+	this.each(function() {
+		if($(this).attr("type") == "checkbox")
+		{
+			var element_value = count;
+			all_elements[count] = $(this);
+			$(this).click(function(event){
+				if(event.shiftKey)
 				{
-					all_elements[initial_element].attr("checked", true);
-					initial_element++;
+					initial_element = element_value > last_clicked ? last_clicked : element_value;
+					final_element = element_value > last_clicked ? element_value : last_clicked;
+					while(initial_element <= final_element)
+						all_elements[initial_element++].attr("checked", true);
 				}
-			}
-			last_clicked = element_value;
-		});
-		count++;
+				last_clicked = element_value;
+			});
+			count++;
+		}
 	});
 }
